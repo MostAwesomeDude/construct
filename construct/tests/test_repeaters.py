5364966528139d4f0b4d51db9c72f168547a19c5
Corbin Simpson <MostAwesomeDude@gmail.com>
2010-12-25 00:05:50 -0800

    tests: Add tests for example from OGR docs.

---
+++ construct/tests/test_repeaters.py
@@ -0,0 +1,23 @@
+import unittest
+
+from construct import OptionalGreedyRepeater, UBInt8
+
+class TestOptionalGreedyRepeater(unittest.TestCase):
+
+    def setUp(self):
+        self.c = OptionalGreedyRepeater(UBInt8("foo"))
+
+    def test_trivial(self):
+        pass
+
+    def test_empty_parse(self):
+        self.assertEqual(self.c.parse(""), [])
+
+    def test_parse(self):
+        self.assertEqual(self.c.parse("\x01\x02"), [1, 2])
+
+    def test_empty_build(self):
+        self.assertEqual(self.c.build([]), "")
+
+    def test_build(self):
+        self.assertEqual(self.c.build([1, 2]), "\x01\x02")
