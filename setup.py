da0d6a2f8903fa5613b5c5bb6b1e01bf87153154
Corbin Simpson <MostAwesomeDude@gmail.com>
2010-12-27 14:19:50 -0800

    Add setup.py and MANIFEST.

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from setuptools import find_packages, setup
+
+setup(
+    name="construct",
+    version="2.04",
+    packages=find_packages(),
+    license="Public Domain",
+    long_description=open("README.rst").read(),
+)
