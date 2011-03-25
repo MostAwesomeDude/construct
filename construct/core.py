93463fb33a933be13d5fdcde79ada490c81ec4b6
Corbin Simpson <MostAwesomeDude@gmail.com>
2011-03-25 01:55:27 -0700

    core: Always raise SizeofError.
    There's an obvious problem here: Stack traces are destroyed by the second
    raise. This might be fine for library usage, but it's kind of unfortunate.
    Hacks to fix this might come later.

--- construct/core.py
+++ construct/core.py
@@ -223,11 +223,23 @@ class Construct(object):
     def sizeof(self, context=None):
         """
         Calculate the size of this object, optionally using a context.
+
+        Some constructs have no fixed size and can only know their size for a
+        given hunk of data; these constructs will raise an error if they are
+        not passed a context.
+
+        :param ``Container`` context: contextual data
+
+        :returns: int of the length of this construct
+        :raises SizeofError: the size could not be determined
         """
 
         if context is None:
             context = AttrDict()
-        return self._sizeof(context)
+        try:
+            return self._sizeof(context)
+        except Exception, e:
+            raise SizeofError(e)
 
     def _sizeof(self, context):
         """
