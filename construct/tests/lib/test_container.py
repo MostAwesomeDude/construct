868a59bc3a2aa0b2f407344f06ad03f45705fc12
Corbin Simpson <MostAwesomeDude@gmail.com>
2011-09-23 21:17:52 -0700

    tests: Test more of Container.
    Man, this object's really got some deep issues.

--- construct/tests/lib/test_container.py
+++ construct/tests/lib/test_container.py
@@ -4,6 +4,24 @@ from construct.lib.container import Container
 
 class TestContainer(unittest.TestCase):
 
+    def test_eq_eq(self):
+        c = Container(a=1)
+        d = Container(a=1)
+        self.assertEqual(c, d)
+
+    def test_ne_wrong_type(self):
+        c = Container(a=1)
+        d = {"a": 1}
+        self.assertNotEqual(c, d)
+
+    def test_bool_false(self):
+        c = Container()
+        self.assertFalse(c)
+
+    def test_bool_true(self):
+        c = Container(a=1)
+        self.assertTrue(c)
+
     def test_in(self):
         c = Container(a=1)
         self.assertTrue("a" in c)
