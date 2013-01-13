5c8c02645c3219e8affee39d71215e01b8d98d17
Tomer Filiba <tomerfiliba@gmail.com>
2013-01-13 15:22:50 +0200

    updates for setup

--- construct/__init__.py
+++ construct/__init__.py
@@ -10,14 +10,12 @@ Hands-on example:
     ...     UBInt8("a"),
     ...     UBInt16("b"),
     ... )
-    >>> s.parse("\\x01\\x02\\x03")
-    Container(a = 1, b = 515)
-    >>> print s.parse("\\x01\\x02\\x03")
+    >>> print s.parse(b"\\x01\\x02\\x03")
     Container:
         a = 1
         b = 515
     >>> s.build(Container(a = 1, b = 0x0203))
-    "\\x01\\x02\\x03"
+    b"\\x01\\x02\\x03"
 """
 
 from construct.core import (AdaptationError, Adapter, Anchor, ArrayError, Buffered, Construct, ConstructError, 
@@ -60,31 +58,22 @@ Embed = Embedded
 # exposed names
 #===============================================================================
 __all__ = [
-    'AdaptationError', 'Adapter', 'Alias', 'Aligned', 'AlignedStruct',
-    'Anchor', 'Array', 'ArrayError', 'BFloat32', 'BFloat64', 'Bit', 'BitField',
-    'BitIntegerAdapter', 'BitIntegerError', 'BitStruct', 'Bits', 'Bitwise',
-    'Buffered', 'Byte', 'Bytes', 'CString', 'CStringAdapter', 'Const',
-    'ConstAdapter', 'ConstError', 'Construct', 'ConstructError', 'Container',
-    'Debugger', 'Embed', 'Embedded', 'EmbeddedBitStruct', 'Enum', 'ExprAdapter',
-    'Field', 'FieldError', 'Flag', 'FlagsAdapter', 'FlagsContainer',
-    'FlagsEnum', 'FormatField', 'GreedyRange', 'HexDumpAdapter', 'If', 'IfThenElse', 
-    'IndexingAdapter', 'LFloat32',
-    'LFloat64', 'LazyBound', 'LengthValueAdapter', 'ListContainer',
-    'MappingAdapter', 'MappingError', 'MetaArray', 'MetaField',
-    'NFloat32', 'NFloat64', 'Nibble', 'NoneOf',
-    'Octet', 'OnDemand', 'OnDemandPointer', 'OneOf',
-    'OpenRange', 'Optional', 'OptionalGreedyRange',
-    'PaddedStringAdapter', 'Padding',
-    'PaddingAdapter', 'PaddingError', 'PascalString', 'Pass', 'Peek',
-    'Pointer', 'PrefixedArray', 'Probe', 'Range', 'RangeError', 'Reconfig',
-    'Rename', 'RepeatUntil', 'Restream', 'SBInt16', 'SBInt32',
-    'SBInt64', 'SBInt8', 'SLInt16', 'SLInt32', 'SLInt64', 'SLInt8', 'SNInt16',
-    'SNInt32', 'SNInt64', 'SNInt8', 'Select', 'SelectError', 'Sequence',
-    'SizeofError', 'SlicingAdapter', 'StaticField', 'String',
-    'StringAdapter', 'Struct', 'Subconstruct', 'Switch', 'SwitchError',
-    'SymmetricMapping', 'Terminator', 'TerminatorError', 'Tunnel',
-    'TunnelAdapter', 'UBInt16', 'UBInt32', 'UBInt64', 'UBInt8', 'ULInt16',
-    'ULInt32', 'ULInt64', 'ULInt8', 'UNInt16', 'UNInt32', 'UNInt64', 'UNInt8',
-    'Union', 'ValidationError', 'Validator', 'Value', "Magic", "this",
-    "OverwriteError"
+    'AdaptationError', 'Adapter', 'Alias', 'Aligned', 'AlignedStruct', 'Anchor', 'Array', 'ArrayError', 
+    'BFloat32', 'BFloat64', 'Bit', 'BitField', 'BitIntegerAdapter', 'BitIntegerError', 'BitStreamReader', 
+    'BitStreamWriter', 'BitStruct', 'Bitwise', 'Buffered', 'CString', 'CStringAdapter', 'ConstAdapter', 
+    'ConstError', 'Construct', 'ConstructError', 'Container', 'Debugger', 'Embedded', 'EmbeddedBitStruct', 
+    'Enum', 'ExprAdapter', 'Field', 'FieldError', 'Flag', 'FlagsAdapter', 'FlagsContainer', 'FlagsEnum', 
+    'FormatField', 'GreedyRange', 'HexDumpAdapter', 'HexString', 'If', 'IfThenElse', 'IndexingAdapter', 
+    'LFloat32', 'LFloat64', 'LazyBound', 'LazyContainer', 'LengthValueAdapter', 'ListContainer', 'Magic', 
+    'MappingAdapter', 'MappingError', 'MetaArray', 'MetaField', 'NFloat32', 'NFloat64', 'Nibble', 'NoneOf', 
+    'Octet', 'OnDemand', 'OnDemandPointer', 'OneOf', 'OpenRange', 'Optional', 'OptionalGreedyRange', 
+    'OverwriteError', 'Packer', 'PaddedStringAdapter', 'Padding', 'PaddingAdapter', 'PaddingError', 
+    'PascalString', 'Pass', 'Peek', 'Pointer', 'PrefixedArray', 'Probe', 'Range', 'RangeError', 'Reconfig', 
+    'Rename', 'RepeatUntil', 'Restream', 'SBInt16', 'SBInt32', 'SBInt64', 'SBInt8', 'SLInt16', 'SLInt32', 
+    'SLInt64', 'SLInt8', 'SNInt16', 'SNInt32', 'SNInt64', 'SNInt8', 'Select', 'SelectError', 'SeqOfOne', 
+    'Sequence', 'SizeofError', 'SlicingAdapter', 'StaticField', 'String', 'StringAdapter', 'Struct', 
+    'Subconstruct', 'Switch', 'SwitchError', 'SymmetricMapping', 'Terminator', 'TerminatorError', 
+    'TunnelAdapter', 'UBInt16', 'UBInt32', 'UBInt64', 'UBInt8', 'ULInt16', 'ULInt24', 'ULInt32', 'ULInt64', 
+    'ULInt8', 'UNInt16', 'UNInt32', 'UNInt64', 'UNInt8', 'Union', 'ValidationError', 'Validator', 'Value', 
+    'this', 'Bits', 'Byte', 'Bytes', 'Const', 'Tunnel', 'Embed',
 ]
--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 import os
-from setuptools import find_packages, setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
 HERE = os.path.dirname(__file__)
 exec(open(os.path.join(HERE, "construct", "version.py")).read())
@@ -8,15 +11,31 @@ exec(open(os.path.join(HERE, "construct", "version.py")).read())
 setup(
     name = "construct",
     version = version_string, #@UndefinedVariable
-    packages = find_packages(),
+    packages = [
+        'construct',
+        'construct.lib', 
+        'construct.formats', 
+        'construct.formats.data', 
+        'construct.formats.executable', 
+        'construct.formats.filesystem', 
+        'construct.formats.graphics',
+        'construct.protocols', 
+        'construct.protocols.application', 
+        'construct.protocols.layer2', 
+        'construct.protocols.layer3', 
+        'construct.protocols.layer4',
+    ],
     license = "MIT",
-    description = "A powerful declarative parser for binary data",
-    long_description = open("README.rst").read(),
+    description = "A powerful declarative parser/builder for binary data",
+    long_description = open(os.path.join(HERE, "README.rst")).read(),
     platforms = ["POSIX", "Windows"],
-    url = "https://github.com/construct/construct",
+    url = "http://construct.readthedocs.org",
     author = "Tomer Filiba, Corbin Simpson",
     author_email = "tomerfiliba@gmail.com, MostAwesomeDude@gmail.com",
     install_requires = ["six"],
+    requires = ["six"],
+    provides = ["construct"],
+    keywords = "construct, data structure, binary, parser, builder, pack, unpack",
     classifiers = [
         "Development Status :: 5 - Production/Stable",
         "License :: OSI Approved :: MIT License",
@@ -27,6 +46,7 @@ setup(
         "Programming Language :: Python :: 3.0",
         "Programming Language :: Python :: 3.1",
         "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
     ],
-    test_suite = "tests",
+    #test_suite = "tests",
 )
