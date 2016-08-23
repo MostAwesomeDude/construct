eb32966441a4286ff337aaa14f69687c6101ade7
Arkadiusz Bulski <arek.bulski@gmail.com>
2016-08-23 03:10:26 +0200

    Tidy up imports in lib/__init__

--- construct/lib/__init__.py
+++ construct/lib/__init__.py
@@ -1,5 +1,4 @@
 from construct.lib.binary import int_to_bin, bin_to_int, swap_bytes, encode_bin, decode_bin
 from construct.lib.bitstream import BitStreamReader, BitStreamWriter
-from construct.lib.container import (Container, FlagsContainer, ListContainer,
-                       LazyContainer)
+from construct.lib.container import Container, FlagsContainer, ListContainer, LazyContainer
 from construct.lib.hex import HexString, hexdump
