2730a250a2c66aa9d192c59b901802785e1419ff
Benjamin Riggs <deisum@gmail.com>
2016-08-24 17:54:26 -0500

    Added tests.

--- tests/test_adapters.py
+++ tests/test_adapters.py
@@ -11,16 +11,17 @@ class TestFlagsAdapter(unittest.TestCase):
 
     def setUp(self):
         self.fa = FlagsAdapter(Byte('HID_type'), {'feature': 4, 'output': 2, 'input': 1})
-        self.byte = b'\x04'
 
     def test_trivial(self):
         pass
 
     def test_parse(self):
-        self.assertEqual(self.fa.parse(self.byte), FlagsContainer(feature=True, output=False, input=False))
+        self.assertEqual(self.fa.parse(b'\x04'), FlagsContainer(feature=True, output=False, input=False))
 
     def test_build(self):
-        self.assertEqual(self.fa.build(dict(feature=True)), self.byte)
+        self.assertEqual(self.fa.build(dict(feature=True, output=True, input=False)), b'\x06')
+        self.assertEqual(self.fa.build(dict(feature=True)), b'\x04')
+        self.assertEqual(self.fa.build(dict()), b'\x00')
 
     def test_build_unknown_flag_given(self):
         self.assertRaises(MappingError, self.fa.build, dict(unknown=True, feature=True))
