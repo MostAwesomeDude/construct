7ea1ac4a870cc9cabeff7e8b99f366f9249f8f40
Arkadiusz Bulski <arek.bulski@gmail.com>
2016-08-25 03:46:08 +0200

    Replaced six.u with own function

--- tests/test_strings.py
+++ tests/test_strings.py
@@ -1,5 +1,8 @@
 import unittest
-import six
+
+import sys
+PY3 = sys.version_info[0] == 3
+def u(s): return s if PY3 else unicode(s.replace(r'\\', r'\\\\'), "unicode_escape")
 
 from construct import String, PascalString, CString, UBInt16, GreedyString
 
@@ -11,7 +14,7 @@ class TestString(unittest.TestCase):
 
     def test_parse_utf8(self):
         s = String("foo", 12, encoding="utf8")
-        self.assertEqual(s.parse(b"hello joh\xd4\x83n"), six.u("hello joh\u0503n"))
+        self.assertEqual(s.parse(b"hello joh\xd4\x83n"), u("hello joh\u0503n"))
 
     def test_parse_padded(self):
         s = String("foo", 10, padchar=b"X", paddir="right")
@@ -31,65 +34,65 @@ class TestString(unittest.TestCase):
 
     def test_build_utf8(self):
         s = String("foo", 12, encoding="utf8")
-        self.assertEqual(s.build(six.u("hello joh\u0503n")), b"hello joh\xd4\x83n")
+        self.assertEqual(s.build(u("hello joh\u0503n")), b"hello joh\xd4\x83n")
 
     def test_build_padded(self):
         s = String("foo", 10, padchar="X", paddir="right", encoding="utf8")
-        self.assertEqual(s.build(six.u("hello")), b"helloXXXXX")
+        self.assertEqual(s.build(u("hello")), b"helloXXXXX")
 
     def test_build_padded_left(self):
         s = String("foo", 10, padchar="X", paddir="left", encoding="utf8")
-        self.assertEqual(s.build(six.u("hello")), b"XXXXXhello")
+        self.assertEqual(s.build(u("hello")), b"XXXXXhello")
 
     def test_build_padded_center(self):
         s = String("foo", 10, padchar="X", paddir="center", encoding="utf8")
-        self.assertEqual(s.build(six.u("hello")), b"XXhelloXXX")
+        self.assertEqual(s.build(u("hello")), b"XXhelloXXX")
 
 
 class TestPascalString(unittest.TestCase):
     def test_parse(self):
         s = PascalString("foo", encoding="utf8")
-        self.assertEqual(s.parse(b"\x05hello"), six.u("hello"))
+        self.assertEqual(s.parse(b"\x05hello"), u("hello"))
 
     def test_build(self):
         s = PascalString("foo", encoding="utf8")
-        self.assertEqual(s.build(six.u("hello world")), b"\x0bhello world")
+        self.assertEqual(s.build(u("hello world")), b"\x0bhello world")
 
     def test_parse_custom_length_field(self):
         s = PascalString("foo", length_field=UBInt16("length"), encoding="utf8")
-        self.assertEqual(s.parse(b"\x00\x05hello"), six.u("hello"))
+        self.assertEqual(s.parse(b"\x00\x05hello"), u("hello"))
 
     def test_build_custom_length_field(self):
         s = PascalString("foo", length_field=UBInt16("length"), encoding="utf8")
-        self.assertEqual(s.build(six.u("hello")), b"\x00\x05hello")
+        self.assertEqual(s.build(u("hello")), b"\x00\x05hello")
 
 
 class TestCString(unittest.TestCase):
     def test_parse(self):
         s = CString("foo", encoding="utf8")
-        self.assertEqual(s.parse(b"hello\x00"), six.u("hello"))
+        self.assertEqual(s.parse(b"hello\x00"), u("hello"))
 
     def test_build(self):
         s = CString("foo", encoding="utf8")
-        self.assertEqual(s.build(six.u("hello")), b"hello\x00")
+        self.assertEqual(s.build(u("hello")), b"hello\x00")
 
     def test_parse_terminator(self):
         s = CString("foo", terminators=b"XYZ", encoding="utf8")
-        self.assertEqual(s.parse(b"helloX"), six.u("hello"))
-        self.assertEqual(s.parse(b"helloY"), six.u("hello"))
-        self.assertEqual(s.parse(b"helloZ"), six.u("hello"))
+        self.assertEqual(s.parse(b"helloX"), u("hello"))
+        self.assertEqual(s.parse(b"helloY"), u("hello"))
+        self.assertEqual(s.parse(b"helloZ"), u("hello"))
 
     def test_build_terminator(self):
         s = CString("foo", terminators=b"XYZ", encoding="utf8")
-        self.assertEqual(s.build(six.u("hello")), b"helloX")
+        self.assertEqual(s.build(u("hello")), b"helloX")
 
 
 class TestGreedyString(unittest.TestCase):
     def test_parse(self):
         s = GreedyString("foo", encoding="utf8")
-        self.assertEqual(s.parse(b"hello\x00"), six.u("hello\x00"))
+        self.assertEqual(s.parse(b"hello\x00"), u("hello\x00"))
 
     def test_build(self):
         s = GreedyString("foo", encoding="utf8")
-        self.assertEqual(s.build(six.u("hello")), b"hello")
+        self.assertEqual(s.build(u("hello")), b"hello")
 
