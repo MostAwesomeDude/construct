13a08ed8091091e703d7824b63e78fa9904c64f0
Benjamin Riggs <deisum@gmail.com>
2016-08-25 19:28:17 -0500

    Moved additional tests for Range onto existing RangeTest object.

--- tests/test_repeaters.py
+++ tests/test_repeaters.py
@@ -1,6 +1,6 @@
 import unittest
 
-from construct import UBInt8
+from construct import UBInt8, Struct, Container
 from construct import Range, Array, GreedyRange, OptionalGreedyRange
 from construct import ArrayError, RangeError
 
@@ -9,6 +9,7 @@ class TestRange(unittest.TestCase):
 
     def setUp(self):
         self.c = Range(3, 7, UBInt8("foo"))
+        self.nested_range = Range(0, 100, Struct('report', UBInt8('id')))
 
     def test_trivial(self):
         pass
@@ -16,20 +17,33 @@ class TestRange(unittest.TestCase):
     def test_parse(self):
         self.assertEqual(self.c.parse(b"\x01\x02\x03"), [1, 2, 3])
         self.assertEqual(self.c.parse(b"\x01\x02\x03\x04\x05\x06"),
-            [1, 2, 3, 4, 5, 6])
+                         [1, 2, 3, 4, 5, 6])
         self.assertEqual(self.c.parse(b"\x01\x02\x03\x04\x05\x06\x07"),
-            [1, 2, 3, 4, 5, 6, 7])
-        self.assertEqual(self.c.parse(b"\x01\x02\x03\x04\x05\x06\x07\x08\x09"),
-            [1, 2, 3, 4, 5, 6, 7])
+                         [1, 2, 3, 4, 5, 6, 7])
+        self.assertEqual(self.nested_range.parse(b'\x01\x02'),
+                         [Container(id=1), Container(id=2)])
 
     def test_build(self):
         self.assertEqual(self.c.build([1, 2, 3, 4]), b"\x01\x02\x03\x04")
+        self.assertEqual(self.nested_range.build([dict(id=i) for i in range(5)]), b'\x00\x01\x02\x03\x04')
+
+    def test_parse_undersized(self):
+        self.assertRaises(RangeError, self.c.parse, b'\x00')
 
     def test_build_undersized(self):
         self.assertRaises(RangeError, self.c.build, [1, 2])
 
+    def test_parse_oversized(self):
+        self.assertEqual(self.c.parse(b"\x01\x02\x03\x04\x05\x06\x07\x08\x09"),
+                         [1, 2, 3, 4, 5, 6, 7])
+
     def test_build_oversized(self):
         self.assertRaises(RangeError, self.c.build, [1, 2, 3, 4, 5, 6, 7, 8])
+
+    def test_build_invalid_build_input(self):
+        self.assertRaises(RangeError, self.c.build, 0)
+        self.assertRaises(RangeError, self.nested_range.build, {'id': 1})
+
 
 class TestArray(unittest.TestCase):
 
@@ -52,6 +66,7 @@ class TestArray(unittest.TestCase):
 
     def test_build_undersized(self):
         self.assertRaises(ArrayError, self.c.build, [5, 6, 7])
+
 
 class TestGreedyRange(unittest.TestCase):
 
