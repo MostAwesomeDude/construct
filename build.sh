661c4f840ea6efb4660cb6aa76aa4cbc7ba5bea5
Arkadiusz Bulski <arek.bulski@gmail.com>
2016-09-02 00:22:50 +0200

    chmoded +x build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,5 @@
 python setup.py register
 python setup.py sdist --formats=zip,gztar upload
 python setup.py bdist_wininst --plat-name=win32 upload
-
 # Make sure you have 'wheel' installed: pip install wheel
 python setup.py bdist_wheel upload 
