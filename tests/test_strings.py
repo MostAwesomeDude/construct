144afc8bd6c8404d91da5ce1a0f492b00a10a8c3
Arkadiusz Bulski <arek.bulski@gmail.com>
2016-09-03 23:05:43 +0200

    Tests prettyfied.

--- tests/test_strings.py
+++ tests/test_strings.py
@@ -6,53 +6,53 @@ from construct import StringError
 
 class TestString(unittest.TestCase):
     def test_parse(self):
-        s = String("foo", 5)
+        s = String("s", 5)
         self.assertEqual(s.parse(b"hello"), b"hello")
 
     def test_parse_utf8(self):
-        s = String("foo", 12, encoding="utf8")
+        s = String("s", 12, encoding="utf8")
         self.assertEqual(s.parse(b"hello joh\xd4\x83n"), u"hello joh\u0503n")
 
     def test_parse_padded_right(self):
-        s = String("foo", 10, padchar=b"X", paddir="right")
+        s = String("s", 10, padchar=b"X", paddir="right")
         self.assertEqual(s.parse(b"helloXXXXX"), b"hello")
 
     def test_parse_padded_left(self):
-        s = String("foo", 10, padchar=b"X", paddir="left")
+        s = String("s", 10, padchar=b"X", paddir="left")
         self.assertEqual(s.parse(b"XXXXXhello"), b"hello")
 
     def test_parse_padded_center(self):
-        s = String("foo", 10, padchar=b"X", paddir="center")
+        s = String("s", 10, padchar=b"X", paddir="center")
         self.assertEqual(s.parse(b"XXhelloXXX"), b"hello")
 
     def test_build(self):
-        s = String("foo", 5)
+        s = String("s", 5)
         self.assertEqual(s.build(b"hello"), b"hello")
 
     def test_build_utf8(self):
-        s = String("foo", 12, encoding="utf8")
+        s = String("s", 12, encoding="utf8")
         self.assertEqual(s.build(u"hello joh\u0503n"), b"hello joh\xd4\x83n")
 
     def test_build_missing_encoding(self):
-        s = String("foo", 5)
+        s = String("s", 5)
         self.assertRaises(StringError, s.build, u"hello")
 
     def test_build_padded_right(self):
-        s = String("foo", 10, padchar=u"X", paddir="right", encoding="utf8")
+        s = String("s", 10, padchar=u"X", paddir="right", encoding="utf8")
         self.assertEqual(s.build(u"hello"), b"helloXXXXX")
-        s = String("foo", 10, padchar=b"X", paddir="right", encoding="utf8")
+        s = String("s", 10, padchar=b"X", paddir="right", encoding="utf8")
         self.assertEqual(s.build(u"hello"), b"helloXXXXX")
 
     def test_build_padded_left(self):
-        s = String("foo", 10, padchar=u"X", paddir="left", encoding="utf8")
+        s = String("s", 10, padchar=u"X", paddir="left", encoding="utf8")
         self.assertEqual(s.build(u"hello"), b"XXXXXhello")
-        s = String("foo", 10, padchar=b"X", paddir="left", encoding="utf8")
+        s = String("s", 10, padchar=b"X", paddir="left", encoding="utf8")
         self.assertEqual(s.build(u"hello"), b"XXXXXhello")
 
     def test_build_padded_center(self):
-        s = String("foo", 10, padchar=u"X", paddir="center", encoding="utf8")
+        s = String("s", 10, padchar=u"X", paddir="center", encoding="utf8")
         self.assertEqual(s.build(u"hello"), b"XXhelloXXX")
-        s = String("foo", 10, padchar=b"X", paddir="center", encoding="utf8")
+        s = String("s", 10, padchar=b"X", paddir="center", encoding="utf8")
         self.assertEqual(s.build(u"hello"), b"XXhelloXXX")
 
     def test_build_too_long(self):
@@ -62,11 +62,11 @@ class TestString(unittest.TestCase):
         self.assertEqual(s.build(b"1234567890"), b"67890")
 
     def test_size(self):
-        s = String("foo", 5)
+        s = String("s", 5)
         self.assertEqual(s.sizeof(), 5)
-        s = String("foo", 12, encoding="utf8")
+        s = String("s", 12, encoding="utf8")
         self.assertEqual(s.sizeof(), 12)
-        s = String("foo", 10, padchar=u"X", paddir="left", encoding="utf8")
+        s = String("s", 10, padchar=u"X", paddir="left", encoding="utf8")
         self.assertEqual(s.sizeof(), 10)
 
 
@@ -95,30 +95,30 @@ class TestPascalString(unittest.TestCase):
 
 class TestCString(unittest.TestCase):
     def test_parse(self):
-        s = CString("foo", encoding="utf8")
+        s = CString("s", encoding="utf8")
         self.assertEqual(s.parse(b"hello\x00"), u"hello")
 
     def test_build(self):
-        s = CString("foo", encoding="utf8")
+        s = CString("s", encoding="utf8")
         self.assertEqual(s.build(u"hello"), b"hello\x00")
 
     def test_parse_terminator(self):
-        s = CString("foo", terminators=b"XYZ", encoding="utf8")
+        s = CString("s", terminators=b"XYZ", encoding="utf8")
         self.assertEqual(s.parse(b"helloX"), u"hello")
         self.assertEqual(s.parse(b"helloY"), u"hello")
         self.assertEqual(s.parse(b"helloZ"), u"hello")
 
     def test_build_terminator(self):
-        s = CString("foo", terminators=b"XYZ", encoding="utf8")
+        s = CString("s", terminators=b"XYZ", encoding="utf8")
         self.assertEqual(s.build(u"hello"), b"helloX")
 
 
 class TestGreedyString(unittest.TestCase):
     def test_parse(self):
-        s = GreedyString("foo", encoding="utf8")
+        s = GreedyString("s", encoding="utf8")
         self.assertEqual(s.parse(b"hello\x00"), u"hello\x00")
 
     def test_build(self):
-        s = GreedyString("foo", encoding="utf8")
+        s = GreedyString("s", encoding="utf8")
         self.assertEqual(s.build(u"hello"), b"hello")
 
