d28da39870f970a15e12328477228077f90cb50e
Arkadiusz Bulski <arek.bulski@gmail.com>
2016-09-06 23:43:27 +0200

    Removed string adapters, and fixed emf example. (FORGOT ONE FILE)

---
+++ tests/formats/graphics/test_emf.py
@@ -0,0 +1,17 @@
+import unittest, os
+ontravis = 'TRAVIS' in os.environ
+
+from construct.lib.container import Container
+from construct.formats.graphics.emf import emf_file
+
+
+class TestEmfFormat(unittest.TestCase):
+
+    def test_parse(self):
+        filename = 'emf1.emf' if ontravis else 'tests/emf1.emf'
+        with open(filename,'rb') as f:
+            data = f.read()
+        obj = emf_file.parse(data)
+        print(obj)
+        print("If you want to see it, make it fail.")
+
