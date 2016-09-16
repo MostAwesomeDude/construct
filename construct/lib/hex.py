a86e2bad1762f2517cae96c5ec2fb0ae57fe913c
Arkadiusz Bulski <arek.bulski@gmail.com>
2016-09-16 21:50:23 +0200

    Tidy

--- construct/lib/hex.py
+++ construct/lib/hex.py
@@ -7,8 +7,17 @@ _printable.update((i, bytes2str(int2byte(i))) for i in range(32, 128))
 
 
 def hexdump(data, linesize):
-    """
-    data is a bytes object. The returned result is a string.
+    r"""
+    Turns bytes into a unicode string of the format:
+
+    >>> print(hexdump(b'0' * 100, 16))
+    0000   30 30 30 30 30 30 30 30 30 30 30 30 30 30 30 30   0000000000000000
+    0010   30 30 30 30 30 30 30 30 30 30 30 30 30 30 30 30   0000000000000000
+    0020   30 30 30 30 30 30 30 30 30 30 30 30 30 30 30 30   0000000000000000
+    0030   30 30 30 30 30 30 30 30 30 30 30 30 30 30 30 30   0000000000000000
+    0040   30 30 30 30 30 30 30 30 30 30 30 30 30 30 30 30   0000000000000000
+    0050   30 30 30 30 30 30 30 30 30 30 30 30 30 30 30 30   0000000000000000
+    0060   30 30 30 30                                       0000
     """
     prettylines = []
     if len(data) < 65536:
@@ -17,7 +26,7 @@ def hexdump(data, linesize):
         fmt = "%%08X   %%-%ds   %%s"
     fmt = fmt % (3 * linesize - 1,)
     for i in range(0, len(data), linesize):
-        line = data[i : i + linesize]
+        line = data[i:i+linesize]
         hextext = " ".join('%02x' % b for b in iterateints(line))
         rawtext = "".join(_printable[b] for b in iterateints(line))
         prettylines.append(fmt % (i, str(hextext), str(rawtext)))
@@ -28,11 +37,15 @@ def hexdump(data, linesize):
 class HexString(bytes):
     r"""
     Represents bytes that will be hex-dumped to a string when its string representation is requested.
+
+    See hexdump().
     """
     def __init__(self, data, linesize=16):
         self.linesize = linesize
+
     def __new__(cls, data, *args, **kwargs):
         return bytes.__new__(cls, data)
+
     def __str__(self):
         if not self:
             return "''"
