14b6e0d17edd710a2e183a9b5f2eacf5c4c4afdc
Arkadiusz Bulski <arek.bulski@gmail.com>
2016-09-16 22:06:53 +0200

    Fixed lib/__init__

--- construct/lib/__init__.py
+++ construct/lib/__init__.py
@@ -2,3 +2,4 @@ from construct.lib.container import Container, FlagsContainer, ListContainer, La
 from construct.lib.binary import integer2bits, bits2integer, bytes2bits, bits2bytes, swapbitslines
 from construct.lib.bitstream import BitStreamReader, BitStreamWriter
 from construct.lib.hex import HexString, hexdump
+from construct.lib.py3compat import PY3, PY26, PYPY, stringtypes, int2byte, byte2int, str2bytes, bytes2str, str2unicode, unicode2str, iteratebytes, iterateints
