6731568a3a95099175bbd87e0349d642f44145f2
Arkadiusz Bulski <arek.bulski@gmail.com>
2016-09-19 07:21:36 +0200

    declarative testing: added alldifferent and atmostone

--- tests/declarativeunittest.py
+++ tests/declarativeunittest.py
@@ -1,8 +1,4 @@
 import unittest
-# import traceback
-# import warnings
-# warnings.filterwarnings("ignore", category=DeprecationWarning)
-# import sys
 
 
 def raises(func, *args, **kw):
@@ -12,4 +8,16 @@ def raises(func, *args, **kw):
         return e.__class__
     else:
         return None
+
+
+def atmostone(*args):
+	return sum(int(bool(x)) for x in args) <= 1
+
+
+def alldifferent(*args):
+	for i,x in enumerate(args):
+		for j,y in enumerate(args):
+			if i != j and x == y:
+				return False
+	return True
 
