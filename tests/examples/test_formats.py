8f71306a3488b00fbc51a3a14d22f92b82244084
Arkadiusz Bulski <arek.bulski@gmail.com>
2016-10-03 18:37:28 +0200

    formats updated

--- tests/examples/test_formats.py
+++ tests/examples/test_formats.py
@@ -15,48 +15,60 @@ ident = lambda x: x
 
 
 
+def common(format, data, obj, size=None):
+    assert format.parse(data) == obj
+    assert format.build(obj) == data
+    # following are guaranteed by the above
+    # assert format.parse(format.build(obj)) == obj
+    # assert format.build(format.parse(data)) == data
+    if size is not None:
+        if isinstance(size, Exception):
+            assert raises(format.sizeof) == size
+        else:
+            assert format.sizeof() == size
+
+def commondump(format, filename):
+    if ontravis:
+        filename = "examples/formats/" + filename
+    if not ontravis:
+        filename = "tests/examples/formats/" + filename
+    with open(filename,'rb') as f:
+        data = f.read()
+    commonbytes(format, data)
+
+def commonhex(format, hexdata):
+    commonbytes(format, binascii.unhexlify(hexdata))
+
+def commonbytes(format, data):
+    obj = format.parse(data)
+    print(obj)
+    data2 = format.build(obj)
+    print(hexlify(data))
+    print(hexlify(data2))
+    # assert hexlify(data2) == hexlify(data)
+
+
+
 class TestFormats(unittest.TestCase):
 
-    def commondump(self, format, filename):
-        if ontravis:
-            filename = "examples/formats/" + filename
-        if not ontravis:
-            filename = "tests/examples/formats/" + filename
-        with open(filename,'rb') as f:
-            data = f.read()
-        self.commonbytes(format, data)
-
-    def commonhex(self, format, hexdata):
-        self.commonbytes(format, binascii.unhexlify(hexdata))
-
-    def commonbytes(self, format, bytesdata):
-        obj = format.parse(bytesdata)
-        print(obj)
-        data = format.build(obj)
-        print(hexlify(bytesdata))
-        print(hexlify(data))
-        # assert hexlify(bytesdata) == hexlify(data)
-
-
-
     def test_png(self):
-        self.commondump(png_file, "sample.png")
+        commondump(png_file, "sample.png")
 
     def test_emf(self):
-        self.commondump(emf_file, "emf1.emf")
+        commondump(emf_file, "emf1.emf")
 
     def test_bitmap(self):
-        self.commondump(bitmap_file, "bitmap1.bmp")
-        self.commondump(bitmap_file, "bitmap4.bmp")
-        self.commondump(bitmap_file, "bitmap8.bmp")
-        self.commondump(bitmap_file, "bitmap24.bmp")
+        commondump(bitmap_file, "bitmap1.bmp")
+        commondump(bitmap_file, "bitmap4.bmp")
+        commondump(bitmap_file, "bitmap8.bmp")
+        commondump(bitmap_file, "bitmap24.bmp")
 
     def test_mbr(self):
-        self.commondump(mbr_format, "mbr1")
+        commondump(mbr_format, "mbr1")
 
     def test_cap(self):
-        self.commondump(cap_file, "cap2.cap")
+        commondump(cap_file, "cap2.cap")
 
     def test_snoop(self):
-        self.commondump(snoop_file, "snoop1")
+        commondump(snoop_file, "snoop1")
 
