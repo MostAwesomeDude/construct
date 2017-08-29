91d958a22f63f3fd06d574f5041e4d42ae8e88f7
Arkadiusz Bulski <arek.bulski@gmail.com>
2017-08-29 23:40:17 +0200

    Tidy build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,4 @@
-#!/bin/sh
 python setup.py register
 python setup.py sdist --formats=zip,gztar upload
 python setup.py bdist_wininst --plat-name=win32 upload
-# Make sure you have 'wheel' installed: pip install wheel
 python setup.py bdist_wheel upload 
