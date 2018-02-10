bd44d4bf6b6c07c4d8b020bc170041124981240c
Arkadiusz Bulski <arek.bulski@gmail.com>
2018-02-10 17:52:38 +0100

    lib/py3compat performance improvements

--- construct/lib/py3compat.py
+++ construct/lib/py3compat.py
@@ -22,36 +22,38 @@ if PY3:
     unicodestringtype = str
     bytestringtype = bytes
 
+    INT2BYTE_CACHE = {i:bytes((i,)) for i in range(256)}
     def int2byte(i):
-        """Converts int (0 through 255) into b'...' character."""
-        return bytes((i,))
+        """Converts integer (0 through 255) into b'...' character."""
+        return INT2BYTE_CACHE[i]
 
     def byte2int(b):
-        """Converts b'...' character into int (0 through 255)."""
+        """Converts b'...' character into integer (0 through 255)."""
         return ord(b)
 
     def str2bytes(s):
-        """Converts '...' str into b'...' bytes. On PY2 they are equivalent."""
+        """Converts '...' string into b'...' string. On PY2 they are equivalent."""
         return s.encode("utf8")
 
     def bytes2str(b):
-        """Converts b'...' bytes into str. On PY2 they are equivalent."""
+        """Converts b'...' string into '...' string. On PY2 they are equivalent."""
         return b.decode("utf8")
 
     def str2unicode(s):
-        """Converts '...' str into u'...' unicode string. On PY3 they are equivalent."""
+        """Converts '...' string into u'...' string. On PY3 they are equivalent."""
         return s
 
     def unicode2str(s):
-        """Converts u'...' string into '...' str. On PY3 they are equivalent."""
+        """Converts u'...' string into '...' string. On PY3 they are equivalent."""
         return s
 
+    ITERATEBYTES_CACHE = {i:bytes((i,)) for i in range(256)}
     def iteratebytes(s):
-        """Iterates though b'...' string yielding characters as b'...' characters. On PY2 iter is the same."""
-        return map(int2byte, s)
+        """Iterates though b'...' string yielding b'...' characters. On PY2 iter is the same."""
+        return (ITERATEBYTES_CACHE[i] for i in s)
 
     def iterateints(s):
-        """Iterates though b'...' string yielding characters as ints. On PY3 iter is the same."""
+        """Iterates though b'...' string yielding integers. On PY3 iter is the same."""
         return s
 
     def reprbytes(b):
@@ -68,36 +70,36 @@ else:
     bytestringtype = str
 
     def int2byte(i):
-        """Converts int (0 through 255) into b'...' character."""
+        """Converts integer (0 through 255) into b'...' character."""
         return chr(i)
 
     def byte2int(s):
-        """Converts b'...' character into int (0 through 255)."""
+        """Converts b'...' character into integer (0 through 255)."""
         return ord(s)
 
     def str2bytes(s):
-        """Converts '...' str into b'...' bytes. On PY2 they are equivalent."""
+        """Converts '...' string into b'...' string. On PY2 they are equivalent."""
         return s
 
     def bytes2str(b):
-        """Converts b'...' bytes into str. On PY2 they are equivalent."""
+        """Converts b'...' string into '...' string. On PY2 they are equivalent."""
         return b
 
     def str2unicode(b):
-        """Converts '...' str into u'...' unicode string. On PY3 they are equivalent."""
+        """Converts '...' string into u'...' string. On PY3 they are equivalent."""
         return b.encode("utf8")
 
     def unicode2str(s):
-        """Converts u'...' string into '...' str. On PY3 they are equivalent."""
+        """Converts u'...' string into '...' string. On PY3 they are equivalent."""
         return s.decode("utf8")
 
     def iteratebytes(s):
-        """Iterates though b'...' string yielding characters as b'...' characters. On PY2 iter is the same."""
+        """Iterates though b'...' string yielding b'...' characters. On PY2 iter is the same."""
         return s
 
     def iterateints(s):
-        """Iterates though b'...' string yielding characters as ints. On PY3 iter is the same."""
-        return map(byte2int, s)
+        """Iterates though b'...' string yielding integers. On PY3 iter is the same."""
+        return (ord(c) for c in s)
 
     def reprbytes(b):
         if isinstance(b, str):
--- tests/lib/test_py3compat.py
+++ tests/lib/test_py3compat.py
@@ -9,7 +9,9 @@ class TestPy3compat(unittest.TestCase):
 
     def test_int_byte(self):
         assert int2byte(5) == b"\x05"
+        assert int2byte(255) == b"\xff"
         assert byte2int(b"\x05") == 5
+        assert byte2int(b"\xff") == 255
         assert all(byte2int(int2byte(i)) == i for i in range(256))
 
     def test_str_bytes(self):
