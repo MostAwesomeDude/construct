036d664a38dff8ec01229cdda3e3eb5585ae13ea
Arkadiusz Bulski <arek.bulski@gmail.com>
2018-02-11 08:40:17 +0100

    tests fixed, examples fixtures removed

--- tests/examples/test_formats.py
+++ tests/examples/test_formats.py
@@ -4,41 +4,39 @@ from construct.lib import *
 from construct.examples.formats import *
 
 
-class TestFormats(unittest.TestCase):
+def test_png():
+    commondump(png_file, "sample.png")
 
-    def test_png(self):
-        commondump(png_file, "sample.png")
+@xfail(reason="Enum no longer supports default")
+def test_emf():
+    commondump(emf_file, "emf1.emf")
 
-    @xfail(reason="Enum no longer supports default")
-    def test_emf(self):
-        commondump(emf_file, "emf1.emf")
+def test_bitmap():
+    commondump(bitmap_file, "bitmap1.bmp")
+    commondump(bitmap_file, "bitmap4.bmp")
+    commondump(bitmap_file, "bitmap8.bmp")
+    commondump(bitmap_file, "bitmap24.bmp")
 
-    def test_bitmap(self):
-        commondump(bitmap_file, "bitmap1.bmp")
-        commondump(bitmap_file, "bitmap4.bmp")
-        commondump(bitmap_file, "bitmap8.bmp")
-        commondump(bitmap_file, "bitmap24.bmp")
+def test_wmf():
+    commondump(wmf_file, "wmf1.wmf")
 
-    def test_wmf(self):
-        commondump(wmf_file, "wmf1.wmf")
+def test_gif():
+    commondump(gif_file, "sample.gif")
 
-    def test_gif(self):
-        commondump(gif_file, "sample.gif")
+def test_mbr():
+    commondump(mbr_format, "mbr1")
 
-    def test_mbr(self):
-        commondump(mbr_format, "mbr1")
+def test_cap():
+    commondump(cap_file, "cap2.cap")
 
-    def test_cap(self):
-        commondump(cap_file, "cap2.cap")
+def test_snoop():
+    commondump(snoop_file, "snoop1")
 
-    def test_snoop(self):
-        commondump(snoop_file, "snoop1")
+def test_pe32():
+    commondump(pe32_file, "python.exe")
+    commondump(pe32_file, "NOTEPAD.EXE")
+    commondump(pe32_file, "sqlite3.dll")
 
-    def test_pe32(self):
-        commondump(pe32_file, "python.exe")
-        commondump(pe32_file, "NOTEPAD.EXE")
-        commondump(pe32_file, "sqlite3.dll")
-
-    @xfail(reason="Enum no longer supports default")
-    def test_elf32(self):
-        commondump(elf32_file, "ctypes.so")
+@xfail(reason="Enum no longer supports default")
+def test_elf32():
+    commondump(elf32_file, "ctypes.so")
