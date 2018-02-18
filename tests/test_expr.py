2c55553e9b7f782673e814a0b403625b326764a0
Arkadiusz Bulski <arek.bulski@gmail.com>
2018-02-18 08:01:33 +0100

    expr tests updated

--- tests/test_expr.py
+++ tests/test_expr.py
@@ -8,7 +8,7 @@ def test_path():
     x = ~((path.foo * 2 + 3 << 2) % 11)
     assert repr(x) == "not ((((path.foo * 2) + 3) << 2) % 11)"
     assert str(x) == "not ((((path['foo'] * 2) + 3) << 2) % 11)"
-    assert not x(dict(foo=7))
+    assert x(dict(foo=7)) == False
 
 def test_this():
     assert repr(this.x) == "this.x"
@@ -33,8 +33,7 @@ def test_this():
             "c2" / Int8ub,
         ),
     )
-    assert this_example.parse(b"\x05helloABXXXX") == Container(length=5)(value=b'hello')(nested=Container(b1=65)(b2=66)(b3=4295))(condition=1482184792)
-    assert this_example.build(dict(length=5, value=b'hello', nested=dict(b1=65, b2=66), condition=1482184792)) == b"\x05helloABXXXX"
+    common(this_example, b"\x05helloABXXXX", Container(length=5)(value=b'hello')(nested=Container(b1=65)(b2=66)(b3=4295))(condition=1482184792))
 
 def test_this_getitem():
     d = Struct(
@@ -45,7 +44,7 @@ def test_this_getitem():
         Check(this.dup1 == 0),
         Check(this.dup2 == 0),
     )
-    assert d.parse(b"\x00") == Container(num=0, dup1=0, dup2=0)
+    common(d, b"\x00", Container(num=0, dup1=0, dup2=0), 1)
 
 def test_functions():
     assert repr(len_(this.x)) == "len_(this.x)"
@@ -63,8 +62,7 @@ def test_functions():
         Check(this.nega == -1),
         Check(abs_(this.nega) == 1),
     )
-    assert example.parse(b"\x03\x07\xff") == dict(items=[3,7], nega=-1)
-    assert example.build(dict(items=[3,7], nega=-1)) == b"\x03\x07\xff"
+    common(example, b"\x03\x07\xff", Container(items=[3,7], nega=-1), 3)
 
 def test_obj():
     assert repr(obj_) == "obj_"
@@ -85,16 +83,20 @@ def test_obj():
     example = Struct(
         "items" / RepeatUntil(obj_ == 255, Byte),
     )
-    assert example.parse(b"\x03\x07\xff") == dict(items=[3,7,255])
-    assert example.build(dict(items=[3,7,255])) == b"\x03\x07\xff"
+    common(example, b"\x03\x07\xff", Container(items=[3,7,255]))
 
 @xfail(reason="faulty implementation, needs fixing")
 def test_list():
     assert repr(list_) == "list_"
+    assert str(list_) == "list_"
     assert repr(list_ == [0, 1, 2]) == "(list_ == [0, 1, 2])"
+    assert str(list_ == [0, 1, 2]) == "(list_ == [0, 1, 2])"
     assert repr(list_[-1]) == "list_[-1]"
+    assert str(list_[-1]) == "list_[-1]"
     assert repr(list_[-1] == 0) == "(list_[-1] == 0)"
+    assert str(list_[-1] == 0) == "(list_[-1] == 0)"
     assert repr(list_[-1] + 1) == "(list_[-1] + 1)"
+    assert str(list_[-1] + 1) == "(list_[-1] + 1)"
     # missing str
 
     assert (list_)(1,[],{}) == []
@@ -108,30 +110,29 @@ def test_list():
     example = Struct(
         "items" / RepeatUntil(list_[-1] == 255, Byte),
     )
-    assert example.parse(b"\x03\x07\xff") == dict(items=[3,7,255])
-    assert example.build(dict(items=[3,7,255])) == b"\x03\x07\xff"
+    common(example, b"\x03\x07\xff", Container(items=[3,7,255]))
 
 @xfail(reason="this expression does not support in operator")
 def test_this_in_operator():
-    st = Struct(
+    d = Struct(
         "if"  / If(this.data     in [1,2,3], Const(b"4")),
         "not" / If(this.data not in [1,2,3], Const(b"5")),
     )
-    assert st.build(dict(data=1)) == b'4'
-    assert st.build(dict(data=7)) == b'5'
+    assert d.build(dict(data=1)) == b'4'
+    assert d.build(dict(data=7)) == b'5'
 
 def test_lambda_in_operator():
-    st = Struct(
+    d = Struct(
          "if"  / If(lambda ctx: ctx.data     in [1,2,3], Const(b"4")),
          "not" / If(lambda ctx: ctx.data not in [1,2,3], Const(b"5")),
     )
-    assert st.build(dict(data=1)) == b'4'
-    assert st.build(dict(data=7)) == b'5'
+    assert d.build(dict(data=1)) == b'4'
+    assert d.build(dict(data=7)) == b'5'
 
 def test_this_shift_operator():
-    st = Struct(
+    d = Struct(
        "a" / Byte,
        "ls" / Computed(this.a << 1),
        "rs" / Computed(this.a >> 1),
     )
-    assert st.parse(b"\x02") == Container(a=2)(ls=4)(rs=1)
+    assert d.parse(b"\x02") == Container(a=2)(ls=4)(rs=1)
