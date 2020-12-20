daee3b8508e828368eb5a76af414f30860978b38
Robie Basak <robie@justgohome.co.uk>
2020-12-20 21:01:25 +0000

    Add test: build_file() with RawCopy without data
    This is the failure case from #888.

--- tests/test_core.py
+++ tests/test_core.py
@@ -930,6 +930,13 @@ def test_rawcopy_issue_358():
     d = Struct("a"/RawCopy(Byte), "check"/Check(this.a.value == 255))
     assert d.build(dict(a=dict(value=255))) == b"\xff"
 
+def test_rawcopy_issue_888(tmpdir):
+    # If you use build_file() on a RawCopy that has only a value defined, then
+    # RawCopy._build may also attempt to read from the file, which won't work
+    # if build_file opened the file for writing only.
+    d = RawCopy(Byte)
+    d.build_file(dict(value=0), filename=tmpdir.join('test'))
+
 def test_byteswapped():
     d = ByteSwapped(Bytes(5))
     common(d, b"12345", b"54321", 5)
