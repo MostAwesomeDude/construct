3be1722b0669374389c14d70b276d1f476fe4cc9
Arkadiusz Bulski <arek.bulski@gmail.com>
2021-01-27 20:52:29 +0100

    test for #888 updated

--- tests/test_core.py
+++ tests/test_core.py
@@ -940,10 +940,8 @@ def test_rawcopy_issue_888():
     # If you use build_file() on a RawCopy that has only a value defined, then
     # RawCopy._build may also attempt to read from the file, which won't work
     # if build_file opened the file for writing only.
-    with tempfile.TemporaryDirectory() as tmpdir:
-        fname = os.path.join(tmpdir, 'test')
-        d = RawCopy(Byte)
-        d.build_file(dict(value=0), filename=fname)
+    d = RawCopy(Byte)
+    d.build_file(dict(value=0), filename="example_888")
 
 def test_byteswapped():
     d = ByteSwapped(Bytes(5))
