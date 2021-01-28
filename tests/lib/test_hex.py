5af15744949e6cb4e09cc11d7d79f94e3217e010
Arkadiusz Bulski <arek.bulski@gmail.com>
2021-01-28 15:13:41 +0100

    test_hexundump_issue_882 added: not a bug

--- tests/lib/test_hex.py
+++ tests/lib/test_hex.py
@@ -5,3 +5,23 @@ from construct.lib.hex import *
 def test_hexdump():
     for i in range(100):
         assert hexundump(hexdump(b"?"*i,32),32) == b"?"*i
+
+def test_hexundump_issue_882():
+    data1 = (hexdump(hexundump(
+"""
+0000   30 31 32 33 34 35 36 5C 0123456\\
+0008   38                      8
+
+""", linesize=8), linesize=8))
+
+    data2 = (hexdump(hexundump(
+"""
+0000   30 31 32 33 34 35 36 37 01234567
+0008   38                      8
+
+""", linesize=8), linesize=8))
+
+    print(data1)
+    print(data2)
+    assert data1 == hexdump(b"0123456\\8", 8)
+    assert data2 == hexdump(b"012345678", 8)
