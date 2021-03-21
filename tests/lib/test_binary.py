e7dfb0c563018cd95f3f487da39cc070d2af9021
Arkadiusz Bulski <arek.bulski@gmail.com>
2021-03-21 03:34:16 +0100

    binary tests refactored and updated

--- tests/lib/test_binary.py
+++ tests/lib/test_binary.py
@@ -5,8 +5,8 @@ from construct.lib.binary import *
 def test_integer2bits():
     assert integer2bits(0, 0) == b""
     assert integer2bits(19, 5) == b"\x01\x00\x00\x01\x01"
-    assert integer2bits(19, 8) == b'\x00\x00\x00\x01\x00\x00\x01\x01'
-    assert integer2bits(19, 3) == b'\x00\x01\x01'
+    assert integer2bits(19, 8) == b"\x00\x00\x00\x01\x00\x00\x01\x01"
+    assert integer2bits(19, 3) == b"\x00\x01\x01"
     assert integer2bits(-13, 5) == b"\x01\x00\x00\x01\x01"
     assert integer2bits(-13, 8) == b"\x01\x01\x01\x01\x00\x00\x01\x01"
     assert raises(integer2bits, 19, -1) == ValueError
@@ -23,8 +23,18 @@ def test_integer2bytes():
     assert raises(integer2bytes, -19, -1) == ValueError
 
 def test_bits2integer():
+    assert bits2integer(b"") == 0
+    assert bits2integer(b"", True) == 0
     assert bits2integer(b"\x01\x00\x00\x01\x01") == 19
     assert bits2integer(b"\x01\x00\x00\x01\x01", True) == -13
+
+def test_bytes2integer():
+    assert bytes2integer(b"") == 0
+    assert bytes2integer(b"", True) == 0
+    assert bytes2integer(b"\x00") == 0
+    assert bytes2integer(b"\x00", True) == 0
+    assert bytes2integer(b"\xff") == 255
+    assert bytes2integer(b"\xff", True) == -1
 
 def test_cross_integers():
     for i in [-300,-255,-100,-1,0,1,100,255,300]:
@@ -34,12 +44,12 @@ def test_cross_integers():
         assert bytes2bits(integer2bytes(i,8)) == integer2bits(i,64)
 
 def test_bytes2bits():
-    assert bytes2bits(b"ab") == b"\x00\x01\x01\x00\x00\x00\x00\x01\x00\x01\x01\x00\x00\x00\x01\x00"
     assert bytes2bits(b"") == b""
+    assert bytes2bits(b"ab") == b"\x00\x01\x01\x00\x00\x00\x00\x01\x00\x01\x01\x00\x00\x00\x01\x00"
 
 def test_bits2bytes():
-    assert bits2bytes(b"\x00\x01\x01\x00\x00\x00\x00\x01\x00\x01\x01\x00\x00\x00\x01\x00") == b"ab"
     assert bits2bytes(b"") == b""
+    assert bits2bytes(b"\x00\x01\x01\x00\x00\x00\x00\x01\x00\x01\x01\x00\x00\x00\x01\x00") == b"ab"
     assert raises(bits2bytes, b"\x00") == ValueError
     assert raises(bits2bytes, b"\x00\x00\x00\x00\x00\x00\x00") == ValueError
 
@@ -48,11 +58,11 @@ def test_swapbytes():
     assert swapbytes(b"abcd") == b"dcba"
 
 def test_swapbytesinbits():
-    assert swapbytesinbits(b"0000000011111111") == b"1111111100000000"
     assert swapbytesinbits(b"") == b""
+    assert swapbytesinbits(b"0000000011111111") == b"1111111100000000"
     assert raises(swapbytesinbits, b"1") == ValueError
 
 def test_swapbitsinbytes():
-    assert swapbitsinbytes(b'') == b''
-    assert swapbitsinbytes(b'\xf0') == b'\x0f'
-    assert swapbitsinbytes(b'\xf0\x00') == b'\x0f\x00'
+    assert swapbitsinbytes(b"") == b""
+    assert swapbitsinbytes(b"\xf0") == b"\x0f"
+    assert swapbitsinbytes(b"\xf0\x00") == b"\x0f\x00"
